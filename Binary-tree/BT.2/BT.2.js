c7cab6ec7d09ab82c4c7f53be2f63dee8b92c297
harsh <kritirout1483@gmail.com>
2024-02-12 23:15:54 +0530

    BT.2 file is modified

--- Binary-tree/BT.2/BT.2.js
+++ Binary-tree/BT.2/BT.2.js
@@ -10,8 +10,11 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
+
 var preorderTraversal = function (root) {
   //recursive way
+  // Time O(n) where n = number of nodes
+  // Space O(n + logh) where h = height of tree (recursive stack space)
   //  function clonePreorderTraversal(root , ansArr){
   //       if(!root) return ansArr;
   //       ansArr.push(root.val)
@@ -21,7 +24,18 @@ var preorderTraversal = function (root) {
   //  }
   //       return clonePreorderTraversal(root , [])
 
-  // iterative way
+  // iterative way [ Morris Traversal ]
+  // Time O(n) where n = number of nodes
+  // Space O(n)
+  // Before moving to the next node, if the current node has a left child,
+  // first connect the rightmost element of the current node's left subtree to the current node.
+  // Then proceed to traverse the left subtree.
+  // After coming from rightmost element of the current node's left subtree to the current node,
+  // undo the additional connection made earlier.
+  // added picture for reference
+  // 1. when connecting the rightmost element of the current node's left subtree to the current node,
+  // ensure push current element to ansArr;
+  // 2. if there is no left child of current node then push it directly into ansArr and procced to right child
   let curr = root;
   let ansArr = [];
   while (curr) {
